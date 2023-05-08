aeef4d9f83fe15d346d3ca2cdf1f02574d5fa137
RyanWD <ryan.saunders86@icloud.com>
2023-05-08 22:34:00 +0100

    Project 2 progress

---
+++ React-8-Projects/Project2/react-project-2/src/components/Header.js
@@ -0,0 +1,9 @@
+import Group from '../assets/Group.png'
+
+export default function Header() {
+    return (
+        <div className='main--header'>
+            <img src={Group} alt="" />
+        </div>
+    )
+}
---
+++ React-8-Projects/Project2/react-project-2/src/components/Nav.js
@@ -0,0 +1,9 @@
+import test from '../assets/Vector.png';
+
+export default function Nav() {
+    return (
+        <nav className='main--nav'>
+            <img src={test} alt="" />
+        </nav>
+    )
+}
