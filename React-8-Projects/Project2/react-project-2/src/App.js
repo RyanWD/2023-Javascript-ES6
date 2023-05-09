9edc704f46797f1ea0cc10595faab8bff00e94bc
RyanWD <ryan.saunders86@icloud.com>
2023-05-09 22:13:03 +0100

    project 2 cards added

--- React-8-Projects/Project2/react-project-2/src/App.js
+++ React-8-Projects/Project2/react-project-2/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Nav from './components/Nav';
+import Cards from './components/Cards';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Header />
+      <Cards />
     </div>
   );
 }
--- React-8-Projects/Project2/react-project-2/src/components/Header.js
+++ React-8-Projects/Project2/react-project-2/src/components/Header.js
@@ -4,6 +4,8 @@ export default function Header() {
     return (
         <div className='main--header'>
             <img src={Group} alt="" />
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
         </div>
     )
 }
