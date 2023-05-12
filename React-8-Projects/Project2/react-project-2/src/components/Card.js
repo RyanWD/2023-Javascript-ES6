8afdcdc40fc4197e1a86c9154241a9800ad7be2b
RyanWD <ryan.saunders86@icloud.com>
2023-05-12 15:35:06 +0100

    card template complete

--- React-8-Projects/Project2/react-project-2/src/components/Card.js
+++ React-8-Projects/Project2/react-project-2/src/components/Card.js
@@ -2,26 +2,28 @@ import star from "../assets/star.png";
 
 export default function Card(props) {
   return (
-    <div className="main--card">
-      <div className="main--image-container">
-        <img src={props.img} alt="" />
-        <div className="main--image-status">SOLD OUT</div>
-      </div>
-      <div className="main--info-container">
-        <div className="main--info-rating">
-          <img src={star} alt="" />
-          <h6>
-            {props.rating}{" "}
-            <span>
-              ({props.reviewCount})-{props.country}
-            </span>
-          </h6>
+    <div className="main--card-container">
+      <div className="main--card">
+        <div className="main--image-container">
+          <img src={props.img} alt="" />
+          <div className="main--image-status">SOLD OUT</div>
         </div>
         <div className="main--info-container">
-          <h6 className="main--info-description">{props.title}</h6>
-          <h6 className="main--info-description">
-            <span>From £{props.price} </span>/ person
-          </h6>
+          <div className="main--info-rating">
+            <img src={star} alt="" />
+            <h6>
+              {props.rating}{" "}
+              <span>
+                ({props.reviewCount})-{props.country}
+              </span>
+            </h6>
+          </div>
+          <div className="main--info-container">
+            <h6 className="main--info-description">{props.title}</h6>
+            <h6 className="main--info-description">
+              <span>From £{props.price} </span>/ person
+            </h6>
+          </div>
         </div>
       </div>
     </div>
