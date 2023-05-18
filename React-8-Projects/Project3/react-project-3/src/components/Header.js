25792f9317b13d16b74078739ea0315000b59e0d
RyanWD <ryan.saunders86@icloud.com>
2023-05-18 22:55:16 +0100

    card data and styles progress

--- React-8-Projects/Project3/react-project-3/src/components/Header.js
+++ React-8-Projects/Project3/react-project-3/src/components/Header.js
@@ -1,6 +1,7 @@
 export default function Header({title}) {
     return (
     <div className="header--container">
+            <i className="fa-solid fa-earth-europe"></i>
              <h1>{title || "Ryan's travel journal"}</h1>
     </div>
     )
---
+++ React-8-Projects/Project3/react-project-3/src/data/destinationData.js
@@ -0,0 +1,13 @@
+export const Destinations = [
+    {
+        id: 1,
+        country: 'Japan',
+        mapsLink: 'https://placeholderurl.com',
+        location: 'Mount Fuji',
+        dateFrom: '12 Jan 2023',
+        dateTo: '24 Jan 2023',
+        description: 'Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains, and summit hikes remain a popular activity. Its iconic profile is the subject of numerous works of art, notably Edo Period prints by Hokusai and Hiroshige.',
+        image: 'Fuji.png'
+
+    }
+]
