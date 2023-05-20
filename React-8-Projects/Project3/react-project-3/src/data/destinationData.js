9e70ec344cb1495c2668d01e9a30fc404394a6d1
RyanWD <ryan.saunders86@icloud.com>
2023-05-20 22:51:40 +0100

    destination data updated 1/2

--- React-8-Projects/Project3/react-project-3/src/data/destinationData.js
+++ React-8-Projects/Project3/react-project-3/src/data/destinationData.js
@@ -9,5 +9,27 @@ export const Destinations = [
         description: 'Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains, and summit hikes remain a popular activity. Its iconic profile is the subject of numerous works of art, notably Edo Period prints by Hokusai and Hiroshige.',
         image: 'Fuji.png'
 
-    }
+    },
+    {
+        id: 2,
+        country: 'Australia',
+        mapsLink: 'https://placeholderurl.com',
+        location: 'Sydney Opera House',
+        dateFrom: '27 May 2023',
+        dateTo: '8 Jun 2023',
+        description: 'The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the foreshore of Sydney Harbour, it is widely regarded as one of the worlds most famous and distinctive buildings and a masterpiece of 20th-century architecture.',
+        image: 'Opera.png'
+
+    },
+    {
+        id: 3,
+        country: 'Japan',
+        mapsLink: 'https://placeholderurl.com',
+        location: 'Mount Fuji',
+        dateFrom: '12 Jan 2023',
+        dateTo: '24 Jan 2023',
+        description: 'Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains, and summit hikes remain a popular activity. Its iconic profile is the subject of numerous works of art, notably Edo Period prints by Hokusai and Hiroshige.',
+        image: 'Fuji.png'
+
+    },
 ]
