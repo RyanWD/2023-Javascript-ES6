75c65f607ae2a2edd36e35e2d48ec91c4fddfd9a
RyanWD <ryan.saunders86@icloud.com>
2023-05-22 23:31:47 +0100

    project 3 complete

--- React-8-Projects/Project3/react-project-3/src/components/Card.js
+++ React-8-Projects/Project3/react-project-3/src/components/Card.js
@@ -1,7 +1,35 @@
 export default function Card(props) {
   return (
     <div className="card--container">
-      <p>{props.data.id}</p>
+      <div className="card--image">
+        <img src={props.data.image} alt="" />
+      </div>
+      <div className="card--info">
+        <div className="card--info-links">
+        <i className="fa-sharp fa-solid fa-location-dot"></i>
+          <h3>{props.data.country}</h3>
+          
+            <a
+              href={props.data.mapsLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View on google maps
+            </a>
+         
+        </div>
+        <div className="card--info-header">
+          <h1>{props.data.location}</h1>
+        </div>
+        <div className="card--info-dates">
+          <h4>
+            {props.data.dateFrom} - {props.data.dateTo}
+          </h4>
+        </div>
+        <div className="card--info-description">
+          <p>{props.data.description}</p>
+        </div>
+      </div>
     </div>
   );
 }
--- React-8-Projects/Project3/react-project-3/src/data/destinationData.js
+++ React-8-Projects/Project3/react-project-3/src/data/destinationData.js
@@ -7,7 +7,7 @@ export const Destinations = [
         dateFrom: '12 Jan 2023',
         dateTo: '24 Jan 2023',
         description: 'Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains, and summit hikes remain a popular activity. Its iconic profile is the subject of numerous works of art, notably Edo Period prints by Hokusai and Hiroshige.',
-        image: 'Fuji.png'
+        image: 'Fuji.jpg'
 
     },
     {
@@ -18,7 +18,7 @@ export const Destinations = [
         dateFrom: '27 May 2023',
         dateTo: '8 Jun 2023',
         description: 'The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the foreshore of Sydney Harbour, it is widely regarded as one of the worlds most famous and distinctive buildings and a masterpiece of 20th-century architecture.',
-        image: 'Opera.png'
+        image: 'Opera.jpg'
 
     },
     {
@@ -29,7 +29,7 @@ export const Destinations = [
         dateFrom: '01 Oct 2023',
         dateTo: '18 Nov 2023',
         description: 'The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality. It is a 15-kilometre-long branch off the Sunnylvsfjorden, which is a branch off the Storfjorden',
-        image: 'Geirangerfjord.png'
+        image: 'Geirangerfjord.jpg'
 
     },
 ]
