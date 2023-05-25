3852a16f24b2d24823dd90f2f2892c226f6389af
RyanWD <ryan.saunders86@icloud.com>
2023-05-25 22:56:41 +0100

    react store inc

--- React-8-Projects/Project4/react-project-4/src/components/Form.js
+++ React-8-Projects/Project4/react-project-4/src/components/Form.js
@@ -1,13 +1,32 @@
+import {memesData} from '../components/MemesData'
+
 export default function Form() {
+  const handleClick = (event) => {
+    event.preventDefault()
+    console.log('yep')
+  }
+
+  const allMemes = memesData.data.memes
+  
+  const randomMeme = () => {
+    const randomNumber = Math.floor(Math.random() * allMemes.length)
+    console.log(randomNumber)
+    const {url} = allMemes[randomNumber]
+    console.log(memeUrl)
+  }
+
+  
   return (
     <div className="form--container">
-      <form action="">
+      <form onSubmit={handleClick}>
         <div className="form--inputs">
         <input type="text"></input>
         <input type="text"></input>
         </div>
-        <button>Get a new meme image</button>
+        <button type="submit">Get a new meme image 🖼</button>
       </form>
+      <button type="button" onClick={randomMeme}>Get a new meme image 🖼</button>
+        <img src={memeUrl ? memeUrl : 'https://i.imgflip.com/30b1gx.jpg'} width="100%" height="auto"/>
     </div>
   );
 }
