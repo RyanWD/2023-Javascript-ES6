4778f6602d0a3a9b37591b9a8771bae6edb89a6c
RyanWD <ryan.saunders86@icloud.com>
2023-06-23 13:37:26 +0100

    testing derived state - giving a component local state - not needed!

--- React-8-Projects/Project4/react-project-4/src/components/Box.js
+++ React-8-Projects/Project4/react-project-4/src/components/Box.js
@@ -1,7 +1,21 @@
-export default function Box(props) {
-    return (
-        <>
-           <div className="box" style={{backgroundColor: props.on && 'black'}}></div> 
-        </>
-    )
+import React from "react";
+
+export default function Box({ on }) {
+  const [box, setBox] = React.useState(on);
+
+  const styles = {
+    backgroundColor: box ? "#222222" : "#cccccc",
+    cursor: "pointer",
+  };
+
+  const switchColor = () => {
+    setBox((prevBox) => !prevBox);
+  };
+
+  return (
+    <>
+      {/* <div className="box" style={{backgroundColor: props.on && 'black'}}></div>  */}
+      <div className="box" style={styles} onClick={switchColor}></div>
+    </>
+  );
 }
--- React-8-Projects/Project4/react-project-4/src/components/SideChallenge.js
+++ React-8-Projects/Project4/react-project-4/src/components/SideChallenge.js
@@ -1,9 +1,13 @@
+import React from 'react'
 import Box from './Box'
 import { Boxes } from './BoxData'
 
 export default function SideChallenge() {
 
-    const allBoxes = Boxes.map((box) => <Box key={box.id} on={box.on}/>)
+    const [boxes, setBoxes] = React.useState(Boxes)
+
+
+    const allBoxes = boxes.map((box) => <Box key={box.id} on={box.on}/>)
     return (
         <>
         <div className="main--container boxes--layout">
