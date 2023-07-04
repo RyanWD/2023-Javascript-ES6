1d02d3287f0dc5d222d496c0fd5a6b5388d0e7a1
RyanWD <ryan.saunders86@icloud.com>
2023-07-04 22:27:01 +0100

    car card icon updates

--- Next-Project-One/car-showcase/components/CarCard.tsx
+++ Next-Project-One/car-showcase/components/CarCard.tsx
@@ -11,38 +11,59 @@ interface CarCardProps {
 }
 
 function CarCard({ car }: CarCardProps) {
+  const { city_mpg, make, model, transmission, year, drive } = car;
 
-
-  const { city_mpg, make, model, transmission, year } = car;
-
-const carRent = calculateCarRent(city_mpg, year)
-
+  const carRent = calculateCarRent(city_mpg, year);
 
   return (
-  <div className="car-card group">
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
 
-    <div className="car-card__content">
-        <h2 className="car-card__content-title">{make} {model}</h2>
-    </div>
-
-    <p className="flex mt-6 text-[32px] font-extrabold">
-        <span className="self-start text-[14px] font-semibold">
-            $
-        </span>
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start text-[14px] font-semibold">$</span>
         {carRent}
-        <span className="self-end text-[14px] font-medium">
-            /day
-        </span>
-    </p>
+        <span className="self-end text-[14px] font-medium">/day</span>
+      </p>
 
-    <div className="relative w-full h-40 my-3 object-contain">
-    <Image src="/hero.png" alt="car model" fill priority className="object-contain"/>
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src="/hero.png"
+          alt="car model"
+          fill
+          priority
+          className="object-contain"
+        />
+      </div>
+
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              width={20}
+              height={20}
+              alt="steering wheel"
+            />
+            <p className="text-[14]">
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tire.svg" width={20} height={20} alt="tire" />
+            <p className="text-[14]">{drive.toUpperCase()}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/gas.svg" width={20} height={20} alt="gas" />
+            <p className="text-[14]">{city_mpg} MPG</p>
+          </div>
+        </div>
+      </div>
     </div>
-
-
-
-  </div>
-  )
+  );
 }
 
 export default CarCard;
