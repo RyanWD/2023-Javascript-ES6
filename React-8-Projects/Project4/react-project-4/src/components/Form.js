4526cbac28f00cf1de840202b8ac04d48c7e5762
RyanWD <ryan.saunders86@icloud.com>
2023-07-17 22:40:52 +0100

    added text to meme gen

--- React-8-Projects/Project4/react-project-4/src/components/Form.js
+++ React-8-Projects/Project4/react-project-4/src/components/Form.js
@@ -1,5 +1,5 @@
 import { memesData } from "../components/MemesData";
-import Image from "../components/Image"
+import Image from "../components/Image";
 import React from "react";
 
 export default function Form() {
@@ -10,23 +10,32 @@ export default function Form() {
     topText: "",
     bottomText: "",
     randomImage: "",
-  })
+  });
 
-  const [memeArray, setMemeArray] = React.useState(memesData.data.memes)
+
+  const [memeArray, setMemeArray] = React.useState(memesData.data.memes);
   // const [meme, setMeme] = React.useState("")
-  const randomMeme = () => {
-    const randomNumber = Math.floor(Math.random() * memeArray.length);
+  const randomMemeText = (event) => {
     // if you need access to previous state (first parameter is current state)
     // setMemeUrl(prevMemeUrl => prevMemeUrl = allMemes[randomNumber].url)
-    setMemeObject(prevMemeObject => {
+    const { name, value } = event.target;
+    setMemeObject((prevMemeObject) => {
       return {
         ...prevMemeObject,
-        randomImage: memeArray[randomNumber]
-
-      }
-    })
-   
+        [name]: value,
+      };
+    });
   };
+
+  const handleImage = () => {
+    const randomNumber = Math.floor(Math.random() * memeArray.length);
+    setMemeObject((prevMemeObject) => {
+      return {
+        ...prevMemeObject,
+        randomImage: memeArray[randomNumber],
+      };
+    });
+  }
 
   // const [firstName, setFirstName] = React.useState("")
   const [person, setPerson] = React.useState({
@@ -36,150 +45,166 @@ export default function Form() {
     comment: "",
     isChecked: true,
     employment: "",
-    favColor: ""
-  })
+    favColor: "",
+  });
 
   const handleInputChange = (event) => {
     const { name, value, type, checked } = event.target;
     setPerson((prevState) => ({
       ...prevState,
-      [name]: type === 'checkbox' ? checked : value
+      [name]: type === "checkbox" ? checked : value,
     }));
   };
 
   const handleSubmit = (event) => {
-      event.preventDefault()
-      console.log(person)
-  }
+    event.preventDefault();
+    console.log(person);
+  };
 
-  console.log(person)
+  console.log(person);
 
   return (
     <div className="form--container">
       <div className="form--inputs">
-        <input type="text" placeholder="Top text"></input>
-        <input type="text" placeholder="Bottom text"></input>
+        <input
+          value={memeObject.topText}
+          name="topText"
+          type="text"
+          placeholder="Top text"
+          onChange={randomMemeText}
+        ></input>
+        <input
+          value={memeObject.bottomText}
+          name="bottomText"
+          type="text"
+          placeholder="Bottom text"
+          onChange={randomMemeText}
+        ></input>
       </div>
-    
 
-      <button type="button" onClick={randomMeme}>
+      <button type="button" onClick={handleImage}>
         Get a new meme image
       </button>
-      <Image memeUrl={memeObject.randomImage.url} key={memeObject.randomImage.id} alt={memeObject.randomImage.name}/>
-
+      <div className="form--display">
+        <p className="top--text">{memeObject.topText}</p>
+        <Image
+          memeUrl={memeObject.randomImage.url}
+          key={memeObject.randomImage.id}
+          alt={memeObject.randomImage.name}
+        />
+        <p className="bottom--text">{memeObject.bottomText}</p>
+      </div>
       <form onSubmit={handleSubmit}>
-      <label style={{color: 'black'}}>
-        First Name:
-        <input
-          type="text"
-          name="firstName"
-          value={person.firstName}
-          onChange={handleInputChange}
-        />
-      </label>
-      <br />
-      <label style={{color: 'black'}}>
-        Last Name: 
-        <input
-          type="text"
-          name="lastName"
-          value={person.lastName}
-          onChange={handleInputChange}
-        />
-      </label>
-      <br />
-      <label style={{color: 'black'}}>
-        Email:
-        <input
-          type="email"
-          name="email"
-          value={person.email}
-          onChange={handleInputChange}
-        />
-      </label>
-      <br />
-      <label htmlFor="textarea" style={{color: 'black'}}>
-        Comment:
-      <textarea  
-          name="comment"
-          id="textarea"
-          value={person.comment}
-          onChange={handleInputChange}/>
-      </label>
-      <label htmlFor="textarea" style={{color: 'black'}}>
-        Are you friendly?:
-      <input
-          type="checkbox" 
-          name="isChecked"
-          checked={person.isChecked}
-          onChange={handleInputChange}/>
-      </label>
+        <label style={{ color: "black" }}>
+          First Name:
+          <input
+            type="text"
+            name="firstName"
+            value={person.firstName}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label style={{ color: "black" }}>
+          Last Name:
+          <input
+            type="text"
+            name="lastName"
+            value={person.lastName}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label style={{ color: "black" }}>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={person.email}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="textarea" style={{ color: "black" }}>
+          Comment:
+          <textarea
+            name="comment"
+            id="textarea"
+            value={person.comment}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label htmlFor="textarea" style={{ color: "black" }}>
+          Are you friendly?:
+          <input
+            type="checkbox"
+            name="isChecked"
+            checked={person.isChecked}
+            onChange={handleInputChange}
+          />
+        </label>
 
-      <br />
-      <br />
-            
-      <fieldset>
+        <br />
+        <br />
+
+        <fieldset>
           <legend>Current employment status</legend>
-          
-          <input 
-              type="radio"
-              id="unemployed"
-              name="employment"
-              value="unemployed"
-              checked={person.employment === 'unemployed'}
-              onChange={handleInputChange}
+
+          <input
+            type="radio"
+            id="unemployed"
+            name="employment"
+            value="unemployed"
+            checked={person.employment === "unemployed"}
+            onChange={handleInputChange}
           />
           <label htmlFor="unemployed">Unemployed</label>
           <br />
-          
-          <input 
-              type="radio"
-              id="part-time"
-              name="employment"
-              value="part-time"
-              checked={person.employment === 'part-time'}
-              onChange={handleInputChange}
 
+          <input
+            type="radio"
+            id="part-time"
+            name="employment"
+            value="part-time"
+            checked={person.employment === "part-time"}
+            onChange={handleInputChange}
           />
           <label htmlFor="part-time">Part-time</label>
           <br />
-          
-          <input 
-              type="radio"
-              id="full-time"
-              name="employment"
-              value="full-time"
-              checked={person.employment === 'full-time'}
-              onChange={handleInputChange}
 
-
+          <input
+            type="radio"
+            id="full-time"
+            name="employment"
+            value="full-time"
+            checked={person.employment === "full-time"}
+            onChange={handleInputChange}
           />
           <label htmlFor="full-time">Full-time</label>
           <br />
-          
-      </fieldset>
+        </fieldset>
 
-      <br />
-            
-            <label htmlFor="favColor">What is your favorite color?</label>
-            <br />
-            <select 
-                id="favColor"
-                value={person.favColor}
-                onChange={handleInputChange}
-                name="favColor"
-            >
-                <option value=""></option>
-                <option value="red">Red</option>
-                <option value="orange">Orange</option>
-                <option value="yellow">Yellow</option>
-                <option value="green">Green</option>
-                <option value="blue">Blue</option>
-                <option value="indigo">Indigo</option>
-                <option value="violet">Violet</option>
-            </select>
+        <br />
 
-            <button>Submit</button>
+        <label htmlFor="favColor">What is your favorite color?</label>
+        <br />
+        <select
+          id="favColor"
+          value={person.favColor}
+          onChange={handleInputChange}
+          name="favColor"
+        >
+          <option value=""></option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="indigo">Indigo</option>
+          <option value="violet">Violet</option>
+        </select>
+
+        <button>Submit</button>
       </form>
     </div>
   );
