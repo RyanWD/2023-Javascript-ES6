7efc024b33a02a5b32f5b9c29a54d8c42aaa36a1
RyanWD <ryan.saunders86@icloud.com>
2023-07-24 21:27:38 +0100

    useEffect continued

--- React-8-Projects/Project5/react-project-5/src/App.js
+++ React-8-Projects/Project5/react-project-5/src/App.js
@@ -1,20 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 function App() {
-  const [starWarsData, setStarWarsData] = useState({})
+  const [starWarsData, setStarWarsData] = useState({});
 
   useEffect(() => {
     fetch("https://swapi.dev/api/people/1")
-        .then(res => res.json())
-        .then(data => setStarWarsData(data))
-  }, [])
-
+      .then((res) => res.json())
+      .then((data) => setStarWarsData(data));
+  }, []);
 
   return (
     <div className="App">
-           <div>
-            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
-        </div>
+      <div>
+        <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+      </div>
     </div>
   );
 }
